68e6d89db8b0aaed13731859f67afd1c24c9d430
luojie <1171156631@qq.com>
2020-08-26 21:29:48 +0800

    修改了readme

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@ import Vue from 'vue'
 
 import app from './App.vue'
 
-// Header - 顶部导航栏  :按需导入
+// Header - 顶部导航栏 : 按需导入
 import { Header } from 'mint-ui';
 Vue.component(Header.name, Header);
+
 
 // Tabbar - 底部选项卡
 import './lib/mui/css/mui.min.css'
